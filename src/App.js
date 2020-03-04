ee604b2018938e2d7a024f492a55eef619131d79
oranye <n.oranye@genesystechhub.com>
2020-03-04 17:56:49 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppRoutes from './AppRoutes'
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import AuthLayout from './components/authLayout/authLayout'
+import Login from './pages/login/login'
+
+
+
+const AuthRoute = ({ Component, path, exact, ...rest }) => {
+    return <Route exact={exact} path={path} {...rest} render={props => {
+        return <AuthLayout><Component /></AuthLayout>
+    }} />
+}
+
+export default function AppRoutes() {
+    return (
+        <Router>
+            <Switch>
+                <AuthRoute path="/" exact Component={Login} />
+                <AuthRoute path="/login" exact Component={Login} />
+            </Switch>
+        </Router>
+
+    )
+}
