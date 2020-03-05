d76378a144209a8323a2849ca6c46bf28e88c679
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2020-03-05 12:33:53 +0100

    Seperated the email and password intocomponents

--- src/pages/authPage/login/login.js
+++ src/pages/authPage/login/login.js
@@ -1,17 +1,10 @@
 import React from 'react';
-// import './login.css'
+import Email from './components/email'
 
-export default function login() {
+export default function Login() {
     return (
-        <div className="login-box">
-            <div className="name-box">
-            <hi className="welcome-text">Welcome Back!</hi>
-            </div>
-            <p className="text">We know you’ve been working hard, let’s measure your progress</p>
-            <form>
-                <input type="email" className="form-control enter-mail" id="email" placeholder="Please enter your company email address"/>
-                <button className="btn proceed">Proceed</button>
-            </form>
+        <div>
+            <Email/>
         </div>
     )
 }
