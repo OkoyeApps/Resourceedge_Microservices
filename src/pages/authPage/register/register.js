6f9269de0596c359d9661d07b3252ce35d481c1f
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2020-03-05 08:09:48 +0100

    Added styles to the login and register page using register.css

---
+++ src/pages/authPage/register/register.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './register.css'
+
+export default function register() {
+    return (
+        <div className="login-box">
+            <div className="name-box">
+            <hi className="welcome-text">Welcome!</hi>
+            </div>
+            <p className="text">We know you’ve been working hard, let’s measure your progress</p>
+            <form>
+                <input type="email" className="form-control enter-mail" id="email" placeholder="Please enter your company email address"/>
+                <button className="btn proceed">Proceed</button>
+            </form>
+        </div>
+    )
+}
