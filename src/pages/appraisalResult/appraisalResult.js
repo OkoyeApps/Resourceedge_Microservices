a2bbcf4df2428b9ad39508be3e83db3c21cc7d56
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2020-03-06 14:31:22 +0100

    Finished the appraisal result page successfully

--- src/pages/appraisalResult/appraisalResult.js
+++ src/pages/appraisalResult/appraisalResult.js
@@ -15,7 +15,38 @@ export default function AppraisalResult() {
                     <button className="btn score-button">N/A</button>
                 </div>
                 <div className="score-breakdown">
-
+                    <div>
+                        <div><p>Learnable Physical Internship</p></div>
+                        <div className="score-upload-progress">
+                            <p>0</p>
+                            <div className="progress progress-adjust"></div>
+                            <p>0 of 56%</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div><p>Social Media</p></div>
+                        <div className="score-upload-progress">
+                            <p>0</p>
+                            <div className="progress progress-adjust"></div>
+                            <p>0 of 14%</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div><p>ICDL</p></div>
+                        <div className="score-upload-progress">
+                            <p>0</p>
+                            <div className="progress progress-adjust"></div>
+                            <p>0 of 25%</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div><p>Fencing and Wiring</p></div>
+                        <div className="score-upload-progress">
+                            <p>0</p>
+                            <div className="progress progress-adjust"></div>
+                            <p>0 of 5%</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="no-appraisal">
                     <p>You haven’t been appraised this quarter. When you get appraised, you’ll see the results here</p>
