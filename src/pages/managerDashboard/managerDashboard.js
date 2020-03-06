5cd98dec01f84ead78919e43a2925a008f3f8317
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2020-03-06 22:44:43 +0100

    Designed the 2nd row of the page

--- src/pages/managerDashboard/managerDashboard.js
+++ src/pages/managerDashboard/managerDashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './managerDashboard.css';
 import Avatar from "react-avatar";
+import add from '../../assets/images/plusIcon.svg';
 
 
 const ManagerDashboard = () => {
@@ -11,11 +12,22 @@ const ManagerDashboard = () => {
                 <div className="row manager-appraisee-name">
                     <div className="manager-appraisee-icon-container">
                         <div className="ic">
-                            <Avatar size={"100px"} name="Elizer Abe" className="rounded-circle"/>
+                            <Avatar size={"100px"} name="Eliezer Ajah" className="rounded-circle"/>
                         </div>
-                        <h2>Elizer Ajah</h2>
+                        <h2>Eliezer Ajah</h2>
                         <h5>Genesys</h5>
                         <p>e.ajah@genesystechhub.com</p>
+                    </div>
+                </div>
+                <div className="row manager-info-list">
+                    <div className="manager-year"><p>Year 2020</p></div>
+                    <div className="manager-period"><p>Period: Q2</p></div>
+                    <div className="manager-appraisal"><p>Appraiser: All</p></div>
+                    <div className="manager-add">
+                        <div>
+                            <img src={add} alt="good icon"  className=""/>
+                            <p>Add Key Result Area</p>
+                        </div>
                     </div>
                 </div>
             </div>
