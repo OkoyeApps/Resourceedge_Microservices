bb331029c5f0539c8c389583025f0675605bf244
Iheanyi25 <38940102+Iheanyi25@users.noreply.github.com>
2020-03-07 07:34:13 +0100

    Completed the managerDashboard page

--- src/pages/managerDashboard/managerDashboard.js
+++ src/pages/managerDashboard/managerDashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './managerDashboard.css';
 import Avatar from "react-avatar";
 import add from '../../assets/images/plusIcon.svg';
+import good from '../../assets/images/Online.svg';
 
 
 const ManagerDashboard = () => {
@@ -25,9 +26,29 @@ const ManagerDashboard = () => {
                     <div className="manager-appraisal"><p>Appraiser: All</p></div>
                     <div className="manager-add">
                         <div>
-                            <img src={add} alt="good icon"  className=""/>
+                            <img src={add} alt="add"  className=""/>
                             <p>Add Key Result Area</p>
                         </div>
+                    </div>
+                </div>
+                <div className="manager-employee-designation">
+                    <div className="employee-designation-box">
+                        <div>
+                            <div>
+                                <h3>Learnable Physical Internship&nbsp;&nbsp;<img src={good} alt="good"  className=""/></h3>
+                            </div>
+                        </div>
+                        <div><p>Expand</p></div>
+                    </div>
+                    <div className="manager-appraiser-details">
+                        <p>Weight: <b>15%</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Appraiser: <b>Ekene Odum</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;HOD: <b>Ekene Odum</b></p>
+                    </div>
+                    <div className="manager-key-outcomes">
+                        <p>Key Outcomes(3)</p>
+                    </div>
+                    <div>
+                        <button type="button" className="btn manager-aprrove-buttons">Edit</button>
+                        <button type="button" className="btn manager-aprrove-buttons">Approve</button>
                     </div>
                 </div>
             </div>
