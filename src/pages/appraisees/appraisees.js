db1dcb5c7d1a76477efb0edc5428bc77e8068eef
oranye <n.oranye@genesystechhub.com>
2020-03-23 14:48:55 +0100

    appraisees api consumed

--- src/pages/appraisees/appraisees.js
+++ src/pages/appraisees/appraisees.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import EpaAvatar from '../../assets/images/epaAvatar.svg'
 import Avatar from 'react-avatar'
 import AppraiseNav from '../../components/appraiseNav/appraiseNav'
 import './appraisees.css'
 import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
 import smallTick from '../../assets/images/small-tick.svg'
+import { GetTeamMembers } from '../../reduxStore/actions/EpaActions'
+import Skeleton from 'react-skeleton-loader'
 
 function Appraisees(props) {
     const [tab, setTab] = useState("view")
+    const [appraisees, setAppraisees] = useState([]);
+    const [loading, setLoading] = useState(false)
     console.log(props)
 
+
+    const getAppraisees = async () => {
+        setLoading(true)
+        await props.GetTeamMembers(1, (success, data) => {
+
+            if (success) {
+                setLoading(false)
+                console.log("data in component", data)
+                setAppraisees(data)
+            } else {
+                setLoading(false)
+                //show error message well
+            }
+        });
+
+    }
 
     const handleLink = () => {
         props.history.push({ pathname: "/appraisees/details" })
     }
+
+    useEffect(() => {
+        getAppraisees()
+
+    }, [])
 
     return (
         <div className="row mx-0 pl-0">
@@ -21,117 +47,53 @@ function Appraisees(props) {
             <div className="col-12 pl-0">
                 <div className="row mx-0 mt-4 pl-0">
 
+                    {loading ? <div className="row mx-0">
+                        <div className="col-4 my-2"><Skeleton width={"22vw"} height="15vh" color={"lightgray"} borderRadius="0" /></div>
+                        <div className="col-4 my-2 px-2"><Skeleton width={"22vw"} height="15vh" color={"lightgray"} borderRadius="0" /></div>
+                        <div className="col-4 my-2"><Skeleton width={"22vw"} height="15vh" color={"lightgray"} borderRadius="0" /></div>
+                    </div> :
+                        appraisees.length > 0 ?
+                            appraisees.map((a, i) => {
+                                return (
+                                    <section className="col-4 my-2" onClick={handleLink}>
+                                        <div className="small-card py-2">
+                                            <div className="row mx-0">
+                                                <div className="col-3 pr-0">
 
-                    <section className="col-4 my-2" onClick={handleLink}>
-                        <div className="small-card py-2">
-                            <div className="row mx-0">
-                                <div className="col-3 pr-0">
+                                                    <div style={{ height: "45px", width: "45px" }}>
+                                                        <Avatar size={"100%"} name={a.fullName} className="rounded-circle" />
+                                                        <div className="ticked">
+                                                            <img src={smallTick} alt="appraised" />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="col-9 pl-1">
+                                                    <div>
+                                                        <div className="card-name">{a.fullName}</div>
+                                                        <div className="group">{a.subgroup.name}</div>
+                                                        <div className="card-email">{a.email}</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+                                )
+                            }) :
+                            <div>No appraisees yet</div>
+                    }
 
-                                    <div style={{ height: "45px", width: "45px" }}>
-                                        <Avatar size={"100%"} name="Eliezer Ajah" className="rounded-circle" />
-                                        <div className="ticked">
-                                            <img src={smallTick} alt="appraised" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-9 pl-1">
-                                    <div>
-                                        <div className="card-name">Eliezer Ajah</div>
-                                        <div className="group">Genesys</div>
-                                        <div className="card-email">e.ajah@genesystechhub.com</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="col-4 my-2" onClick={handleLink}>
-                        <div className="small-card py-2">
-                            <div className="row mx-0">
-                                <div className="col-3 pr-0">
-                                    <div style={{ height: "45px", width: "45px" }}>
-                                        <Avatar size={"100%"} name="Eliezer Ajah" className="rounded-circle" />
-                                        <div className="ticked">
-                                            <img src={smallTick} alt="appraised" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-9 pl-1">
-                                    <div>
-                                        <div className="card-name">Eliezer Ajah</div>
-                                        <div className="group">Genesys</div>
-                                        <div className="card-email">e.ajah@genesystechhub.com</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="col-4 my-2" onClick={handleLink}>
-                        <div className="small-card py-2">
-                            <div className="row mx-0">
-                                <div className="col-3 pr-0">
-                                    <div style={{ height: "45px", width: "45px" }}>
-                                        <Avatar size={"100%"} name="Eliezer Ajah" className="rounded-circle" />
-                                        <div className="ticked">
-                                            <img src={smallTick} alt="appraised" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-9 pl-1">
-                                    <div>
-                                        <div className="card-name">Eliezer Ajah</div>
-                                        <div className="group">Genesys</div>
-                                        <div className="card-email">e.ajah@genesystechhub.com</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="col-4 my-2" onClick={handleLink}>
-                        <div className="small-card py-2">
-                            <div className="row mx-0">
-                                <div className="col-3 pr-0">
-                                    <div style={{ height: "45px", width: "45px" }}>
-                                        <Avatar size={"100%"} name="Eliezer Ajah" className="rounded-circle" />
-                                        <div className="ticked">
-                                            <img src={smallTick} alt="appraised" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-9 pl-1">
-                                    <div>
-                                        <div className="card-name">Eliezer Ajah</div>
-                                        <div className="group">Genesys</div>
-                                        <div className="card-email">e.ajah@genesystechhub.com</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    <section className="col-4 my-2" onClick={handleLink}>
-                        <div className="small-card py-2">
-                            <div className="row mx-0">
-                                <div className="col-3 pr-0">
-                                    <div style={{ height: "45px", width: "45px" }}>
-                                        <Avatar size={"100%"} name="Eliezer Ajah" className="rounded-circle" />
-                                        <div className="ticked">
-                                            <img src={smallTick} alt="appraised" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-9 pl-1">
-                                    <div>
-                                        <div className="card-name">Eliezer Ajah</div>
-                                        <div className="group">Genesys</div>
-                                        <div className="card-email">e.ajah@genesystechhub.com</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+
                 </div>
             </div>
         </div>
     )
 }
 
-export default withRouter(Appraisees)
+// const mapStateToProps = (state) => {
+//     console.log("state from map", state)
+//     return {
+//         appraisees: state.epaReducer.result
+//     }
+// }
+
+export default connect(null, { GetTeamMembers })(withRouter(Appraisees))
--- src/reduxStore/actions/EpaActions.js
+++ src/reduxStore/actions/EpaActions.js
@@ -1,15 +1,23 @@
 import RequestProcessor from '../../api/requestProcessor';
+import Constants from '../constants/index'
 
 
-const GetTeamMembers = (userid) => (dispatch, getState) => {
+const GetTeamMembers = (userid, callback) => (dispatch, getState) => {
     RequestProcessor.Get("/team/1", "", (success, header, status, data) => {
         console.log("data returned", success, header, status, data);
-    });
+        if (success) {
+            callback(success, data);
+        } else {
+            callback(success, data);
+        }
+        // dispatch({ type: Constants.Appraisees, payload: data })
+    })
+
 };
 
 // const Get
 
-export {GetTeamMembers}
+export { GetTeamMembers }
 
 
 
