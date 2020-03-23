20d0c13c8c6dca7254cb8bd7a8c702972fdeea90
unknown <arthurbosah41@gmail.com>
2020-03-23 17:07:36 +0100

    fixed selective dropdown

--- src/components/selectiveSearch/selectiveSearch.js
+++ src/components/selectiveSearch/selectiveSearch.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './selectiveSearch.css'
 
 export default function Selector(props) {
-    var { dropClass, dropStyle, tagClass, tagStyle, searchClass, searchStyle, result, isSearchable } = props
+    var { dropClass, dropStyle, tagClass, tagStyle, searchClass, searchStyle, result, isSearchable, disableInput } = props
     let $ = window;
     const [data, setData] = useState([]);
     const [show, setShow] = useState(false);
 
     var searchable = isSearchable ? isSearchable : false
-    
+
     const tellCode = (e) => {
         var inputValue = document.querySelector('#search');
         if (inputValue.value === '') {
@@ -23,7 +24,6 @@ export default function Selector(props) {
                 return;
             }
         }
-
 
         if ($.event.keyCode === 13) {
             if (e.target.value.trim() !== "") {
@@ -50,33 +50,37 @@ export default function Selector(props) {
     const handleSelect = (e) => {
         setData([...data, e.target.innerText])
         document.querySelector('#search').value = " "
+        setShow(false)
     }
-
+    console.log(searchable,"lalalalal",show,)
     return (
         <div>
-            <div className="drop" id="mainInput" style={{ display: 'flex' }} onKeyPress={(e) => { tellCode(e) }}>
-                <ul>
+            <div className="drop w-100" id="mainInput" style={{ display: 'flex' }} onKeyPress={(e) => { tellCode(e) }}>
+                <ul className="w-100">
                     {
                         data &&
                         data.map((item, index) => {
-                            return <li key={index} style={{ ...tagStyle }}><span className={tagClass ? tagClass : "capsule"}>{item} <span onClick={() => { handleDelete(item) }} className="fa fa-times-circle"></span></span></li>
+                            return <li key={index} style={{ ...tagStyle }} className="w-100"><div className={tagClass ? tagClass : "w-100 d-flex justify-content-between form-control"}>{item} <span onClick={() => { handleDelete(item) }}><i class="fas fa-times-circle"></i></span></div></li>
                         })
                     }
-                    <li style={data.length === 0 ? { width: "100%" } : { width: "auto" }}><input type="text" id='search' style={{ ...searchStyle }} className={searchClass ? searchClass : "search"} placeholder={"Please search"} onChange={tellCode} autoFocus={true} /></li>
+                    {
+                        data.length >= 1 ?
+                            ""
+                            :
+                            <li style={data.length === 0 ? { width: "100%" } : { width: "auto" }}><input disabled={disableInput} type="text" id='search' className={searchClass ? searchClass : "form-control"} placeholder={"Please search"} onChange={tellCode} autoFocus={true} autoComplete={false} /></li>
+                    }
                 </ul>
             </div>
             {
                 searchable ?
-                    show ? <div className={dropClass ? dropClass : "dropDown"} style={{ ...dropStyle }}>
-                        <ul>
-                            {result && result.map((item, index) => (<li onClick={handleSelect} key={index}>{item.name}</li>))}
+                    show ? <div className={dropClass ? dropClass : "dropDown w-100"} style={{ ...dropStyle }}>
+                        <ul className="w-100">
+                            {result && result.map((item, index) => (<li onClick={handleSelect} key={index} className="w-100">{item.name}</li>))}
                         </ul>
                     </div> : <></>
                     : <></>
             }
 
-
         </div>
-
     )
 }
--- src/components/twoStepEpaForm/twoStepEpaForm.js
+++ src/components/twoStepEpaForm/twoStepEpaForm.js
@@ -14,12 +14,6 @@ function TwoStepEpaForm(props) {
     return (
         <div className="row mx-0">
             <KeyResultAreaComponent setNextView={setNext} />
-<<<<<<< HEAD
-            <section id="key-result-area-details" className={`col-7 py-3 ${next ? "active-step" : "inactive-krad"}`}>
-                <p className="steps">Step 2<span className="small-half">/2</span></p>
-                <p className="kra-sm-text">Now enter the following details for each Key Results Area on the left respectively</p>
-=======
->>>>>>> 2b2cdb5c158437da521bf24c18abc033f7e12fc6
 
             <KeyResultDetails setNext={setNext} next={next} />
         </div>
