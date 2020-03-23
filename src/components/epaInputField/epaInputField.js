db22055fbb4019be76000cec5ad67968573ce6d8
oranye <n.oranye@genesystechhub.com>
2020-03-23 10:44:03 +0100

    listing kra component finished

--- src/components/epaInputField/epaInputField.js
+++ src/components/epaInputField/epaInputField.js
@@ -2,44 +2,46 @@ import React, { useState, useEffect } from 'react';
 import remove from '../../assets/images/remove.svg';
 
 
-const EpaInputField = ({setTotalWeight, currentTotalWeight, myIndex, setAllKeyResultArea, AllKeyResultAreas, forceUpdate}) => {
+const EpaInputField = ({ setTotalWeight, currentTotalWeight, myIndex, setAllKeyResultArea, AllKeyResultAreas, forceUpdate, c }) => {
+
+    console.log("c", c)
     const removeInputComponent = () => {
         AllKeyResultAreas.splice(myIndex, 1)
-       setAllKeyResultArea(Array.from(AllKeyResultAreas));
+        setAllKeyResultArea(Array.from(AllKeyResultAreas));
     }
 
     const AddNewResultArea = (e) => {
         var tempObj = AllKeyResultAreas[myIndex];
         var currentObj = tempObj;
-        if(e.target.name === 'weight') {
+        if (e.target.name === 'weight') {
             var currentWeight = isNaN(parseInt(tempObj.weight)) ? 0 : parseInt(tempObj.weight)
-            var reducedWeight = currentTotalWeight -  currentWeight;
-            if(reducedWeight > 0){
-                var weightToAdd =  isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value)
-                    var nextTotalWeight = reducedWeight + weightToAdd;
-                    if(nextTotalWeight <= 100){
-                        setTotalWeight(nextTotalWeight);
-                    }else{
-                        e.target.value = 0
-                        var result = currentTotalWeight - currentWeight
-                        setTotalWeight(result)
-                    }
-                }else{
-                    setTotalWeight(isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value));
+            var reducedWeight = currentTotalWeight - currentWeight;
+            if (reducedWeight > 0) {
+                var weightToAdd = isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value)
+                var nextTotalWeight = reducedWeight + weightToAdd;
+                if (nextTotalWeight <= 100) {
+                    setTotalWeight(nextTotalWeight);
+                } else {
+                    e.target.value = 0
+                    var result = currentTotalWeight - currentWeight
+                    setTotalWeight(result)
                 }
+            } else {
+                setTotalWeight(isNaN(parseInt(e.target.value)) ? 0 : parseInt(e.target.value));
             }
-            currentObj = {...currentObj, [e.target.name] : e.target.value}
-            AllKeyResultAreas[myIndex] = currentObj;
-             setAllKeyResultArea(AllKeyResultAreas)
+        }
+        currentObj = { ...currentObj, [e.target.name]: e.target.value }
+        AllKeyResultAreas[myIndex] = currentObj;
+        setAllKeyResultArea(AllKeyResultAreas)
     }
 
     const renderComponet = () => {
-               return (
-               <article className="d-flex kra-inputs mb-2" id="kra-0">
-                    <input type="text" name="name" className="form-control mr-2 kra-textbox"  onChange={AddNewResultArea} />
-                    <input type="number" name="weight" min="0" step="1" className="form-control kra-percent" placeholder="00%" onChange={AddNewResultArea} />
-                    <div onClick={removeInputComponent}><img src={remove} alt="remove kra" className="remove-kra mt-2 ml-1" /></div>
-               </article>)
+        return (
+            <article className="d-flex kra-inputs mb-2 px-3" id="kra-0">
+                <input type="text" name="name" className="form-control mr-2 kra-textbox" onChange={AddNewResultArea} Value={AllKeyResultAreas[myIndex].name} />
+                <input type="number" name="weight" min="0" step="1" className="form-control kra-percent" placeholder="00%" onChange={AddNewResultArea} Value={AllKeyResultAreas[myIndex].weight} />
+                <div onClick={removeInputComponent}><img src={remove} alt="remove kra" className="remove-kra mt-2 ml-1" /></div>
+            </article>)
     }
 
     return (
--- src/components/twoStepEpaForm/twoStepEpaForm.js
+++ src/components/twoStepEpaForm/twoStepEpaForm.js
@@ -13,7 +13,7 @@ function TwoStepEpaForm(props) {
     return (
         <div className="row mx-0">
             <KeyResultAreaComponent setNextView={setNext} />
-           
+
             <section id="key-result-area-details" className={`col-7 py-3 ${next ? "active-step" : "inactive-krad"}`}>
                 <p className="steps">Step 2<span className="small-half">/2</span></p>
                 <p className="kra-sm-text">Now enter the following details for each Key Results Area on the left respectively</p>
@@ -21,11 +21,11 @@ function TwoStepEpaForm(props) {
                 <article className="d-flex pt-3">
                     <div className="mr-4">
                         <label className="form-label kra-sm-text">Appraiser</label>
-                        <input type="text" className="form-control" disabled />
+                        <input type="text" className="form-control" disabled={next ? false : true} />
                     </div>
                     <div>
                         <label className="form-label kra-sm-text">Head of Department</label>
-                        <select className="form-control" disabled>
+                        <select className="form-control" disabled={next ? false : true} >
                             <option></option>
                         </select>
                     </div>
@@ -34,11 +34,11 @@ function TwoStepEpaForm(props) {
                 <article className="d-flex pt-3">
                     <div className="mr-4">
                         <label className="form-label">Key Outcomes</label>
-                        <input type="text" className="form-control" disabled />
+                        <input type="text" className="form-control" disabled={next ? false : true} />
                     </div>
                     <div>
                         <label className="form-label">Timeline</label>
-                        <input type="text" className="form-control" disabled />
+                        <input type="text" className="form-control" disabled={next ? false : true} />
                     </div>
                 </article>
             </section>
