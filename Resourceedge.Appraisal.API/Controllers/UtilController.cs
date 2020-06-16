ebbaa89adfbe547f2158bcb7b6c0496d9acead59
Emmanuel Okoye <developeradmin@genesystechhub.com>
2020-06-16 15:49:58 +0100

    email template for docker bug fix

--- Resourceedge.Appraisal.API/Controllers/UtilController.cs
+++ Resourceedge.Appraisal.API/Controllers/UtilController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using Resourceedge.Appraisal.API.Interfaces;
 using Resourceedge.Appraisal.Domain.Models;
+using Resourceedge.Email.Api.Interfaces;
+using Resourceedge.Email.Api.Model;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,29 +22,39 @@ namespace Resourceedge.Appraisal.API.Controllers
         private readonly IKeyResultArea resultArea;
         private readonly IAppraisalResult appResult;
         private readonly IMapper mapper;
+        private readonly IEmailSender emailSender;
 
-        public UtilController(IKeyResultArea _resultArea, IAppraisalResult _appResult, IMapper _mapper)
+        public UtilController(IKeyResultArea _resultArea, IAppraisalResult _appResult, IMapper _mapper, IEmailSender emailSender)
         {
             resultArea = _resultArea;
             this.appResult = _appResult;
             this.mapper = _mapper;
+            this.emailSender = emailSender;
         }
 
         [Route("check/email/template/for/valid"), HttpGet]
         public async Task<IActionResult> CheckEmailTemplate()
         {
-            //var temp = Path.Combine(Directory.GetCurrentDirectory(), "EmailTemplate/emailTemplateScore.html");
-            //var fileInfo = new FileInfo(temp);
-            //var body = "";
-            //var bbbb = fileInfo.Directory;
-            ////if (fileInfo.Exists)
-            ////{
-            ////    using (StreamReader stream = new StreamReader(fileInfo.FullName))
-            ////    {
-            ////        body = await stream.ReadToEndAsync();
-            ////    }
+            var temp = Path.Combine(Directory.GetCurrentDirectory(), "EmailTemplate/emailTemplateScore.html");
+            var fileInfo = new FileInfo(temp);
+            var body = "";
+            var bbbb = fileInfo.Directory;
 
-            ////}
+           var htmlCOntent =   emailSender.FormatEmailAppraisalScore("Emmanuel", "2.5");
+
+            SingleEmailDto email = new SingleEmailDto();
+            email.ReceiverEmailAddress = "okoyeemma442@gmail.com";
+            email.HtmlContent =  await htmlCOntent;
+          var bbbbb =   await  emailSender.SendToSingleEmployee("Testing on Docker", email);
+            //emailSender.HtmlContent = await sender.FormatEmail(employee.FullName, keyResultArea.AppraiserDetails.Name, msg, title, url);
+            //if (fileInfo.Exists)
+            //{
+            //    using (StreamReader stream = new StreamReader(fileInfo.FullName))
+            //    {
+            //        body = await stream.ReadToEndAsync();
+            //    }
+
+            //}
             //resultArea.SendApprovalNotification(new List<KeyResultArea> { new KeyResultArea { EmployeeId = 194, HodDetails = new NameEmail { Email = "c.okoye@genesystechhub.com" } } });
 
 
